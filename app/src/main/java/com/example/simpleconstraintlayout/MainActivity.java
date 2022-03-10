eb11c68ad45eeaa7d8863986a36a79f0ec3d91c6
Rizvaldi Firnandasyah <rizvaldi108@gmail.com>
2022-03-10 18:41:09 +0700

    update

--- app/src/main/java/com/example/simpleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/simpleconstraintlayout/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.simpleconstraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Deklarasi variabel untuk button
+    Button btnLogin;
+
+    //Deklarasi variabel untuk EditText
+    EditText edemail, edpassword;
+
+    //Deklarasi variabel untuk menyimpan email dan password
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Menghubungkan variabel btnLogin dengan componen button pada layout
+        btnLogin=findViewById(R.id.btSignin);
+
+        //Menghubungkan variabel edemail dengan componen button pada layout
+        edemail=findViewById(R.id.edEmail);
+
+        //Menghubungkan variabel edpassword dengan componen button pada layout
+        edpassword=findViewById(R.id.edPassword);
+
+        //Membuat fungsi onclick pada button btnLogin
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Menyimpan input user di edittext email kedalam variabel nama
+                nama = edemail.getText().toString();
+
+                //Menyimpan input user di edittext password kedalam variabel password
+                password = edpassword.getText().toString();
+
+                //Membuat variabel toast dan membuat toast dengan menambahkan variabel nama dan password
+                Toast t = Toast.makeText(getApplicationContext(),
+                        "email anda: "+nama+" dan Password anda: "+password+"", Toast.LENGTH_LONG);
+                t.show();
+            }
+        });
     }
 }
